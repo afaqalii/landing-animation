2e0b71ef6cd1cb48890f692c1c9874896a22afbd
afaqalii <96257586+afaqalii@users.noreply.github.com>
2022-08-12 09:27:52 +0500

    website uploaded

---
+++ index.js
@@ -0,0 +1,5 @@
+let tl = gsap.timeline({defaults:{ease:'power2.out'}})
+
+tl.to('.slider', {y:'-100%', duration:2})
+tl.to('main', {y:0}, {duration:'-=1'})
+tl.fromTo('nav', {opacity:0},{opacity:1})
