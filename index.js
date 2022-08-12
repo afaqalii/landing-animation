1ec913b1725f64e45908139d9be1c1702c06f445
afaqalii <96257586+afaqalii@users.noreply.github.com>
2022-08-12 09:56:53 +0500

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let tl = gsap.timeline({defaults:{ease:'power2.out'}})
 
-tl.to('.slider', {y:'-100%', duration:2})
-tl.to('main', {y:0}, {duration:'-=.3'})
+tl.fromTo('.slider', {opacity:50},{opacity:0,duration:2})
+tl.to('main', {y:0})
 tl.fromTo('nav', {opacity:0},{opacity:1})
